71c8fe1bcaa471089a180b14ab77aff084d20733
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-08-03 18:24:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <div>
+  <md-field>
+    <md-input 
+    v-model="currentTodo" 
+    @keydown.enter="addTodo()" 
+    placeholder="Add a todo"></md-input>
+  </md-field>
+
+    <ul class="todos">
+      <li v-for="todo in todos" :key="todo.id">
+            <input
+            class="checkbox"
+            v-model="todo.completed" 
+            @change="toggleCompleted()" 
+            placeholder="done">
+
+        {{ todo.label }}
+
+        <button @click="removeTodo(todo)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      currentTodo: ''
+    };
+  },
+  methods: {
+
+    addTodo() {
+      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+      this.currentTodo = '';
+    },
+    editedTodo() {
+      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+      this.currentTodo = '{{ todo.label }}';
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    toggleCompleted() {
+        this.todos.push({id: this.todos.length, label: this.currentTodo, completed: true});
+      this.currentTodo = '';
+    },
+  },
+};
+
+</script>
+
+<style>
+</style>
