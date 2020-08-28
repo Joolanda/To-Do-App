3ee9006f7400a20157bad1865f3ca3d50faf1a12
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-08-28 23:05:45 +0200

    Update template

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,124 @@
 <template>
-
-  <div>
-  <md-field>
-    <md-input 
-    v-model="currentTodo" 
-    @keydown.enter="addTodo()" 
-    placeholder="Add a todo"></md-input>
+  <div class="app-container">
+    <md-toolbar>
+      <h1>{{title}}</h1>
+    </md-toolbar>
+ 
+  <md-field class="addTodo">
+    <md-textarea
+      v-model="currentTodo" 
+      @keydown.enter="addTodo()" 
+      placeholder="Add a new task">
+    </md-textarea>
   </md-field>
+     <md-list class="todos">
+      <md-list-item class="list-item" v-for="todo in todos" :key="todo.id">
+      <md-checkbox type="checkbox" v-model="todo.completed"></md-checkbox>
+        <span v-on:click.prevent="clickToEdit(todo)" v-show="editTodoId !== todo.id" :class="{ completed: todo.completed }"> 
+            {{ todo.label }} 
+          </span> 
+        <input v-model="todo.label" v-show="editTodoId == todo.id" v-on:keyup.enter="saveEdit">
+           <a @click.prevent="removeTodo(todo)">
+             <i class="material-icons right teal-text">delete</i>
+          </a>         
+      </md-list-item>
+    </md-list>
 
-    <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id">
-            <input
-            class="checkbox"
-            v-model="todo.completed" 
-            @change="toggleCompleted()" 
-            placeholder="done">
-
-        {{ todo.label }}
-
-        <button @click="removeTodo(todo)">X</button>
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'RegularToolbar',
   data() {
     return {
+      title: "My Weekly Tasks",
       todos: [],
-      currentTodo: ''
+      currentTodo: '',
+      editTodoId: null
     };
+  },
+  watch: {
+    todos: {
+      handler() {
+        localStorage.todos = JSON.stringify(this.todos);
+      },
+      deep: true,
+    },
+  },
+  mounted() {
+    if (localStorage.todos) {
+      this.todos = JSON.parse(localStorage.todos);
+    }
   },
   methods: {
 
+    showTodos () {
+      return this.todos.length > 0 
+    },
+
+
     addTodo() {
-      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+      if(this.currentTodo !== ''){
+      this.todos.push({
+        id: this.todos.length, 
+        label: this.currentTodo, 
+        completed: false
+        });
       this.currentTodo = '';
+      }
     },
-    editedTodo() {
-      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
-      this.currentTodo = '{{ todo.label }}';
-    },
-    removeTodo(todo) {
+    removeTodo(todo){
       var index = this.todos.indexOf(todo);
       this.todos.splice(index, 1);
     },
-    toggleCompleted() {
-        this.todos.push({id: this.todos.length, label: this.currentTodo, completed: true});
-      this.currentTodo = '';
+    clickToEdit (todo) {
+      this.editTodoId = todo.id; 
     },
-  },
+    saveEdit(){
+      this.editTodoId = null;
+    }
+    
+  }
 };
 
 </script>
 
 <style>
+.addTodo{
+  margin: 0 auto;
+  margin-bottom: 20px;
+  width: 80%;
+  height: 80px;
+}
+.todos {
+  width: 80%;
+  margin: 0 auto;
+  padding-top: 0px;
+  padding-bottom: 0px;
+    border: 0.5px solid dark-grey;
+}
+.todo-title{
+  background: #212121;
+}
+.list-item{
+width: 100%;
+}
+.md-field {
+  border: 2px solid blue !important;
+  height: 70px !important;
+  border-radius: 3px !important;
+  width: 60% !important;
+  font-size: 40px !important;
+  min-height: 0px !important;
+  padding-top: 5px !important;
+  padding-left: 5px !important;
+}
+.md-button{
+  border: 1.5px solid white !important;
+}
+.completed{
+  text-decoration: line-through;
+  opacity: 0.5;
+};
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
 import App from './App.vue'
-import { MdField } from 'vue-material/dist/components'
+import { MdButton, MdContent, MdTabs, MdField } from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+Vue.use(VueMaterial)
 
 Vue.use(MdField);
 
